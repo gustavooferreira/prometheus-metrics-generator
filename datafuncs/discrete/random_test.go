e666ad6fa8a7b54a07dd23372211501891d42bb2
Gustavo Ferreira <gustavojcoferreira@gmail.com>
2023-07-28 00:26:11 +0100

    Add random data function to discrete space (#15)

---
+++ datafuncs/discrete/random_test.go
@@ -0,0 +1,41 @@
+package discrete_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/gustavooferreira/prometheus-metrics-generator/datafuncs/discrete"
+)
+
+func TestRandomDataIterator(t *testing.T) {
+	t.Run("should fail given that iteration count limit is set to zero", func(t *testing.T) {
+		_, err := discrete.NewRandom(discrete.RandomOptions{
+			AmplitudeMin: 11,
+			AmplitudeMax: 20,
+		})
+		require.Error(t, err)
+		expectedErrorMessage := "iteration count limit cannot be less than or equal to zero"
+		assert.Equal(t, expectedErrorMessage, err.Error())
+	})
+
+	t.Run("should produce valid results for the given count", func(t *testing.T) {
+		dataGenerator, err := discrete.NewRandom(discrete.RandomOptions{
+			AmplitudeMin:        11,
+			AmplitudeMax:        20,
+			IterationCountLimit: 10,
+		})
+		require.NoError(t, err)
+
+		results := helperScraper(t, dataGenerator.Iterator())
+
+		require.Equal(t, 10, len(results))
+
+		// assert all values are within amplitude range provided
+		for _, result := range results {
+			assert.GreaterOrEqual(t, 20.0, result.scrapeResult.Value)
+			assert.LessOrEqual(t, 11.0, result.scrapeResult.Value)
+		}
+	})
+}
